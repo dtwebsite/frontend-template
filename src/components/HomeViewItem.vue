49190aee9da4b0c9ac6442a84a6088e2be52d485
dennis <a0981038610@gmail.com>
2023-06-23 03:22:20 +0800

    add login

---
+++ src/components/HomeViewItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <table class="divide-y divide-gray-200">
+    <thead>
+      <th v-for="(title, index) in titles" :key="index">{{ title }}</th>
+    </thead>
+    <tr class="py-4 flex" v-for="team in teams" :key="team.id">
+      <td>
+        <img :src="team.image" alt="" />
+      </td>
+      <td>
+        <p>{{ team.name }}</p>
+      </td>
+      <td>
+        <p>{{ team.email }}</p>
+      </td>
+    </tr>
+  </table>
+</template>
+
+<script>
+export default {
+  props: {
+    teams: Array,
+    titles: Array,
+  },
+};
+</script>
